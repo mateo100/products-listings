ab59fff94c20413924e223177477df4c7ac7d038
mateo100 <mat.kar.98@gmail.com>
2022-08-18 12:52:41 +0200

    init repo

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <p>dsads</p>
+    <q-btn color="primary" label="Primary" />
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "LayoutDefault",
+
+  mounted() {
+    console.log("test");
+  },
+};
+</script>
